728a5f7bd0b1f9967a22803dfc50b75cfafe52f9
Busra Ciftlik <busraciftlik96@gmail.com>
2023-05-16 18:30:35 +0300

    added exception handler

---
+++ src/main/java/com/busraciftlik/api/controllers/InvoiceController.java
@@ -0,0 +1,11 @@
+package com.busraciftlik.api.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/invoices")
+public class InvoiceController {
+
+}
