1ff3d514b03dd8be642e10a2a8f32e3193bdfddd
Busra Ciftlik <busraciftlik96@gmail.com>
2023-05-16 18:18:29 +0300

    added new classes

--- src/main/java/com/busraciftlik/ECommerceApplication.java
+++ src/main/java/com/busraciftlik/ECommerceApplication.java
@@ -2,10 +2,11 @@ package com.busraciftlik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableCaching
 @SpringBootApplication
-@EnableTransactionManagement
 public class ECommerceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/busraciftlik/common/constants/ExceptionType.java
@@ -0,0 +1,7 @@
+package com.busraciftlik.common.constants;
+
+public class ExceptionType {
+    public static final String Validation = "VALIDATION_EXCEPTION";
+    public static final String Business = "BUSINESS_EXCEPTION";
+    public static final String Runtime = "RUNTIME_EXCEPTION";
+}
